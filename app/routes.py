fffbde4163227224e792a4fef42948e0477abd6f
antroytheiz <theisandatu@gmail.com>
2020-12-08 12:51:23 +0900

    second commit

--- app/routes.py
+++ app/routes.py
@@ -106,3 +106,106 @@ def pribadiDel(nrp):
     mysql.connection.commit()
     cur.close()
     return redirect(url_for('dashboard'))
+
+# Akhir Pribadi
+
+
+# Awal Polsek
+@app.route('/polsek/')
+def polsek():
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        SELECT * FROM Polsek;
+    ''')
+    data = []
+    for kd,ktp,alamatPol,namaPol in cur.fetchall():
+        data.append((kd,ktp,alamatPol,namaPol))
+    cur.close()
+    print(data)
+    return render_template('polsek/polsek.html', title='Polsek', submenu='dataPolsek', link1='Data Polsek' ,data=data)
+
+# Akhir Polsek
+
+# Awal MedcenITS
+
+@app.route('/medcen/')
+def medcen():
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        SELECT * FROM MedcenITS;
+    ''')
+    data = []
+    for ktp,alamatMed in cur.fetchall():
+        data.append((ktp,alamatMed))
+    cur.close()
+    print(data)
+    return render_template('medcen/medcen.html', title='MedcenITS', submenu='dataMedcenITS', link1='Data MedcenITS' ,data=data)
+
+# Akhir MedcenITS
+
+
+# Awal Prosedur
+@app.route('/prosedur/')
+def prosedur():
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        SELECT * FROM ProsedurLapor;
+    ''')
+    data = []
+    for kd,pLp,pLm in cur.fetchall():
+        data.append((kd,pLp,pLm))
+    cur.close()
+    print(data)
+    return render_template('prosedur/prosedur.html', title='Prosedur Lapor', submenu='dataProsedur Lapor', link1='Data Prosedur Lapor' ,data=data)
+
+# Akhir Prosedur
+
+
+# Awal nLayanan
+@app.route('/nomor_layanan/')
+def layanan():
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        SELECT * FROM NomorLayanan;
+    ''')
+    data = []
+    for kd,nLp,nLm,nDp in cur.fetchall():
+        data.append((kd,nLp,nLm,nDp))
+    cur.close()
+    print(data)
+    return render_template('nomorlayanan/nLayanan.html', title='Nomor Layanan', submenu='dataNomor Layanan', link1='Data Nomor Layanan' ,data=data)
+
+# Akhir nLayanan
+
+
+# Awal WebResmi
+@app.route('/website_resmi/')
+def webResmi():
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        SELECT * FROM WebsiteResmi;
+    ''')
+    data = []
+    for kd,WebsiteResmiPol,WebsiteResmiMed in cur.fetchall():
+        data.append((kd,WebsiteResmiPol,WebsiteResmiMed))
+    cur.close()
+    print(data)
+    return render_template('webResmi/webResmi.html', title='Data Website Resmi', submenu='data Web Resmi', link1='Data Web Resmi' ,data=data)
+
+# Akhir WebResmi
+
+# Awal DataLapor
+@app.route('/data_lapor/')
+def dataLapor():
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        SELECT * FROM DataLapor;
+    ''')
+    data = []
+    for nama,kejadian,nophone,lBer in cur.fetchall():
+        data.append((nama,kejadian,nophone,lBer))
+    cur.close()
+    print(data)
+    return render_template('dataLapor/lapor.html', title='Data Lapor', submenu='data Lapor', link1='Data Lapor' ,data=data)
+
+# Akhir DataLapor
